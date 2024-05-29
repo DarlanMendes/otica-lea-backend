5b64f33196880dff06c118323a30315c8c5de94a
darlan <dedsondarlan@gmail.com>
2024-05-29 01:01:42 -0300

    hello world

--- src/infrastructure/http/routes/UserRoutes.ts
+++ src/infrastructure/http/routes/UserRoutes.ts
@@ -3,7 +3,7 @@ import { UserController } from "./../controllers/userController";
 
 const router = Router();
 const userController = new UserController();
-
+router.get("/", (req, res)=>res.json({msg:"hello world"}))
 router.post("/users", (req, res) => userController.create(req, res));
 router.get("/users/:id", (req, res)=> userController.findById(req, res))
 router.get("/users", (req, res)=>userController.findAll(req, res))
