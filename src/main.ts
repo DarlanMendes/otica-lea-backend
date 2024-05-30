7d74080baef25d177ff97a49020e1bac7f7df717
darlan <dedsondarlan@gmail.com>
2024-05-29 23:50:58 -0300

    Adjustement port

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { app } from "./interfaces/express/server";
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
